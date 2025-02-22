bd96438c82918766529b0f3c89d9ecb380f5061f
anushri-choubey04 <anushrichoubey04@gmail.com>
2025-02-22 17:28:57 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+
 // https://vite.dev/config/
 export default defineConfig({
   base: "/React-e-commerce/",
-  plugins: [react(), tailwindcss()
-    
-  ],
-  
-    "@tailwindcss/postcss": {},
-  
+  plugins: [react(), tailwindcss(), ],
+
+  "@tailwindcss/postcss": {},
+
   resolve: {
     alias: {
       "@": "/src",
