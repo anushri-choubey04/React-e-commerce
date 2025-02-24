8ebead072f7da03982ab9b7b28653a98264577dd
anushri-choubey04 <anushrichoubey04@gmail.com>
2025-02-24 17:26:48 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-
+import process from 'node:process';
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/React-e-commerce/",
+  base: process.env.NODE_ENV === "production" ? "/React-e-commerce/" : "/",
+  
   plugins: [react(), tailwindcss()],
 
   "@tailwindcss/postcss": {},
